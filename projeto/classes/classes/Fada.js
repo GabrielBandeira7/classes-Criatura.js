0e15b8fbe7b0c816e7c33d7a02307177f81c9b13
GabrielBandeira7 <bandeiragabriel07@gmail.com>
2025-08-25 17:10:07 -0300

    Update Fada.js

--- projeto/classes/classes/Fada.js
+++ projeto/classes/classes/Fada.js
@@ -1,25 +1,15 @@
+const Criatura = require("./Criatura");
+
 class Fada extends Criatura{
-    #energia;
-  
-    constructor(nome) {
-      this.nome = nome;
-      this.ataqueMagico = "Bola de Luz Arco-Íris";
-      this.#energia = 100; 
-    }
-  
-    agir() {
-      if (this.#energia >= 15) {
-        console.log(`${this.nome} ataca com ${this.ataqueMagico}!`);
-        this.#energia -= 15;
-      } else {
-        console.log(`${this.nome} está sem energia para atacar.`);
-      }
-    }
-  
-   
-    getEnergia() {
-      return this.#energia;
-    }
+  constructor(nome){
+    super(nome); 
+    this.ataqueMagico = "Bola de Luz Arco-Íris";
   }
-  
-  module.exports = Fada;
+
+  agir(){
+    console.log(`${this.nome} ataca com ${this.ataqueMagico}!`);
+    this.alterarEnergia(-15);
+  }
+}
+
+module.exports = Fada;
